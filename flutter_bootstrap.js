4d3d9db314ee34def35f8ca14630a77231de8a1d
Maxxetto <71521859+Maxxetto@users.noreply.github.com>
2026-08-18 22:37:40 +0200

    Publish Kasper guide and web simulation fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3355349881" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1525910996" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
